a08c233470d0e24d510454445f64dfb0e45d8517
itisansh <anshkumarvishwakarma11a5@gmail.com>
2026-06-24 11:02:42 +0530

    Internship code also added

---
+++ JS Internship/JS Basics/script.js
@@ -0,0 +1,10 @@
+// primitive data types
+let age = 24; //number
+let str = String(age); //string
+const isMale = true;
+const aadhar = 38484930033n; //bigint
+const mobile = Symbol("336665545");
+let course; //undefined
+// const b; //error
+
+console.log(typeof aadhar);
